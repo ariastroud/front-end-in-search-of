1fbfa932e50836b5219628bb4ae79c96ad61d691
Aria Stroud <stroudaria@gmail.com>
2023-01-25 17:55:06 -0800

    Login with google button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,47 @@ import "./App.css";
 // import { signInWithGoogle } from "./Firebase";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
-import Login from "./components/Login";
+// import Login from "./components/Login";
+// import { useEffect } from "react";
+import React from "react";
+// import { GoogleAuthProvider } from "firebase/auth";
+import { GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [loginData, setLoginData] = useState("");
+
   return (
     <div className="App">
       <Navbar />
-      <Login />
-      {/* <button onClick={signInWithGoogle}>Sign In With Google</button> */}
+      {!loginData ? (
+        <GoogleLogin
+          className="loginButton"
+          onSuccess={(credentialResponse) => {
+            // console.log(credentialResponse.credential);
+            var user = jwt_decode(credentialResponse.credential);
+            // console.log(user.email);
+            // console.log(user.name);
+            const name = user.name;
+            const email = user.email;
+            const requestBody = { name: name, email: email };
+
+            return axios
+              .post(`http://localhost:5000/users`, requestBody)
+              .then((response) => {
+                // console.log(response.data[0]);
+                setLoginData(response.data[0]);
+              });
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      ) : (
+        <section>Hello {loginData.name}</section>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="495498728917-r66b30p22sct1agt5ueo4041553aovdb.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
